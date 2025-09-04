9a34a5f85eb726308be9e009ff33bc36983d950b
m1ggyccs <amigsdavid@gmail.com>
2025-09-04 14:35:27 +0800

    First commit

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ const server = http.createServer((req, res) => {
      });
 
      const PORT = process.env.PORT || 3000;
-     server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+     server.listen(PORT, () => console.log('server running on port ${PORT}`));
